56dd8ada06f9d0f81385279f0505774667c1f0b6
TheDigitalSystem <macidzadeh@gmail.com>
2023-09-09 18:20:16 +0400

    App is Ready! fine

--- src/main/java/azingress/Controller/CarController.java
+++ src/main/java/azingress/Controller/CarController.java
@@ -16,35 +16,31 @@ public class CarController {
 
     private final CarServiceImpl carServiceImpl;
 
-@GetMapping
-public List<CarResponseDto> findAll(){
-    return  carServiceImpl.findAll();
-}
-
+    @GetMapping
+    public List<CarResponseDto> findAll() {
+        return carServiceImpl.findAll();
+    }
 
 
     @PostMapping() //bu hisseni tuta bilmedim, sual verersen
-    public void create(@RequestBody CarRequestDto carRequestDto){
+    public void create(@RequestBody CarRequestDto carRequestDto) {
         carServiceImpl.createCar(carRequestDto);
     }
 
     @GetMapping("/{id}") // bu hissenide tam tutmadim
-public CarResponseDto getCarByID(@PathVariable Long id){
-        return  carServiceImpl.getCarById(id);
+    public CarResponseDto getCarByID(@PathVariable Long id) {
+        return carServiceImpl.getCarById(id);
     }
 
-@GetMapping("/{id}")
-public void update(@PathVariable Long id, @RequestBody CarRequestDto carRequestDto){
-    carServiceImpl.update(id, carRequestDto);
-}
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody CarRequestDto carRequestDto) {
+        carServiceImpl.update(id, carRequestDto);
+    }
 
-@DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
-    carServiceImpl.delete(id);
-}
-
-
-
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        carServiceImpl.delete(id);
+    }
 
 
 }
